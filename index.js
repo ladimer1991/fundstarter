69fa60260ed665e01980f88a9d4ce13693b2ef4e
Dustin Tran <dustin_tran@live.com>
2016-03-18 00:40:06 +0000

    Part2 Completed

--- index.js
+++ index.js
@@ -2,13 +2,24 @@ var http = require('http');
 
 var file = require('fs');
 var requestListener = function (req, res) {
-  res.writeHead(200);
-  var info = file.readFileSync('ladidadi.html', 'utf8', function read(err, data){
+   res.writeHead(200);
+  
+   file.stat('ladidadi.html', function (err, stats){
       if(err){
          return console.log(err);
       }
-  res.end('info');
-  });
+      file.open('ladidadi.html', 'r', function (err, fd){
+          if(err){
+             return console.log(err);
+          }
+          file.read(fd, new Buffer(stats.size), 0, stats.size, 0, function (err, bytesRead, buffer){
+              if(err){
+                  return console.log(err);
+              }
+              res.end(buffer.toString('utf8'));
+          });
+      });
+   });     
 }
 
 var server = http.createServer(requestListener);
