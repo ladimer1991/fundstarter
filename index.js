f3815f597477c34160757058f305389d42b75597
Dustin Tran <dustin_tran@live.com>
2016-03-18 00:27:40 +0000

    First Commit for Part1B

--- index.js
+++ index.js
@@ -3,8 +3,12 @@ var http = require('http');
 var file = require('fs');
 var requestListener = function (req, res) {
   res.writeHead(200);
-  var info = file.readFileSync('ladidadi.html');
+  var info = file.readFileSync('ladidadi.html', 'utf8', function read(err, data){
+      if(err){
+         return console.log(err);
+      }
   res.end('info');
+  });
 }
 
 var server = http.createServer(requestListener);
